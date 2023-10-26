0ebd311b6767a7a56ce3047b3d669733b7a802a9
reteua <info@realestate.te.ua>
2023-10-26 22:56:18 +0300

    Change #27

--- _includes/js/realtyua.js
+++ _includes/js/realtyua.js
@@ -4,21 +4,5 @@ $("body").tooltip({selector:'[data-toggle="tooltip"]'});
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); });
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
-var settings = {
-    create: false,
-    maxOptions: 10,
-    maxItems: 1,
-    sortField: 'text',
-    render: {
-		no_results:function(data,escape){
-			return '<div class="no-results">За цим запитом "'+escape(data.input)+'" нічого не знайдено</div>';
-		}
-    },
-    onChange: function(value) {
-        if (value !== '') {
-            window.location = value;
-        }
-    }
-};
-new TomSelect('#areaSelect',settings); 
+var settings = { create: false, maxOptions: 10, maxItems: 1, sortField: 'text', render: {	no_results:function( data,escape ) {	return '<div class="no-results">За цим запитом "'+escape(data.input)+'" нічого не знайдено</div>'; } }, onChange: function(value) { if (value !== '') { window.location = value; } } }; new TomSelect('#areaSelect',settings);
 });
